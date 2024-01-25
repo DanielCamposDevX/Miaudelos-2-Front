ebbc85faac3b82f2324123775aaa300816f6711f
Daniel Campos <daniel.campos@devio.com.br>
2024-01-24 22:00:06 -0300

    feat/ Signup screen style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,83 @@
+"use client"
 import Image from "next/image"
 import Cat from "@/assets/cat.jpg"
+import CatBack from "@/assets/gatinhos.png"
+import { motion } from 'framer-motion';
 import LoginForm from "@/components/login/form"
+import SignupForm from "@/components/signup/form"
+import { useEffect, useState } from "react";
 
 export default function Login() {
 
+    const [login, setLogin] = useState(true);
+    const [animateY, setAnimateY] = useState(false);
+    const [animateX, setAnimateX] = useState(false);
+
+
+
+    function handleChangeForm() {
+        if (!login) {
+            setAnimateY(true)
+            setTimeout(() => {
+                setAnimateX(false);
+                setLogin(true);
+                setTimeout(() => { setAnimateY(false) }, 500);
+            }, 500);
+        }
+        if (login) {
+            setAnimateY(true)
+            setTimeout(() => {
+                setAnimateX(true);
+                setLogin(false);
+                setTimeout(() => { setAnimateY(false) }, 500);
+            }, 500);
+        }
+    }
 
 
 
     return (
-        <main className="w-full h-screen flex justify-center items-center bg-purple-300">
-            <div className="rounded-3xl flex h-5/6  w-11/12 lg:w-9/12 border bg-white shadow-lg overflow-hidden">
-                <div className="hidden lg:flex w-1/2 h-full">
-                    <Image alt="gatinho"
-                        style={{ objectFit: "cover" }}
-                        quality={100}
-                        src={Cat} />
-                </div>
-                <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
-                    <LoginForm />
-                </div>
+        <main className="w-full h-screen flex justify-center items-center">
+            <Image
+                alt="gatinho"
+                style={{ objectFit: "cover", position: "absolute", zIndex: 0, width: "100%", height: "100%" }}
+                quality={100}
+                src={CatBack}
+            />
+            <div className="rounded-3xl flex h-5/6 w-11/12 lg:w-9/12 border-4 bg-white/90 border-yellow-300 shadow-2xl overflow-hidden z-10">
+                <motion.div
+                    className="hidden lg:flex w-1/2 h-full"
+                    initial={{ x: "0%" }}
+                    animate={{ x: login ? "0%" : "100%" }}
+                    exit={{ x: "0%" }}
+                    transition={{ duration: 1, ease: "easeInOut" }}
+                >
+                    <Image alt="gatinho" style={{ objectFit: "cover", zIndex: 20 }} quality={100} src={Cat} />
+                </motion.div>
+                <motion.div
+                    className="hidden lg:flex w-full lg:w-1/2 flex-col justify-center items-center"
+                    initial={{ y: "-100%" }}
+                    animate={{ y: animateY ? "-100%" : "0%", x: animateX ? "-100%" : "0%" }}
+                    exit={{ y: "100%" }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                    <div className="w-full h-full flex justify-center items-center">
+                        {login && <LoginForm setLogin={handleChangeForm} />}
+                        {!login && <SignupForm setLogin={handleChangeForm} />}
+                    </div>
+                </motion.div>
+                <motion.div
+                    className="flex lg:hidden w-full lg:w-1/2 flex-col justify-center items-center"
+                    initial={{ y: "-100%" }}
+                    animate={{ x: "0%", y: animateY ? "-100%" : "0%" }}
+                    exit={{ y: "100%" }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                    <div className="w-full h-full flex justify-center items-center">
+                        {login && <LoginForm setLogin={handleChangeForm} />}
+                        {!login && <SignupForm setLogin={handleChangeForm} />}
+                    </div>
+                </motion.div>
             </div>
         </main>
     )
--- src/components/signup/form.tsx
+++ src/components/signup/form.tsx
@@ -6,7 +6,7 @@ import Logo from "@/assets/LogoName.png"
 import { Button } from "@mui/material";
 
 
-export default function LoginForm({ setLogin }: any) {
+export default function SignupForm({ setLogin }: any) {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -15,19 +15,22 @@ export default function LoginForm({ setLogin }: any) {
 
 
     return (
-        <form className="flex flex-col w-4/5 lg:w-3/5 h-full items-center justify-around pt-10 z-10">
+        <form className="flex flex-col w-4/5 lg:w-3/5 h-full items-center justify-around py-5 gap-6  z-10 ">
             <Image alt="gatinho"
                 style={{ objectFit: "cover" }}
                 quality={100}
                 src={Logo} />
             <div className="flex flex-col gap-5 w-full items-center text-purple-400 font-bold">
-                Bem Vindo de Volta!
+                Cadastre-se aqui!
+                <Input label={"CPF"} type={"Email"} value={email} setValue={setEmail} />
                 <Input label={"Email"} type={"Email"} value={email} setValue={setEmail} />
                 <Input label={"Senha"} type={"password"} value={password} setValue={setPassword} />
+                <Input label={"Confirme a senha"} type={"password"} value={password} setValue={setPassword} />
+
             </div>
             <Button variant="contained" sx={{ padding: "10px 20px" }}>Entrar</Button>
             <div className="flex justify-center items-center text-sm">
-                Ou se preferir crie sua conta clicando
+                Já tem uma conta? Clique
                 <a onClick={() => { setLogin() }} className="text-purple-400 hover:cursor-pointer"> "aqui"</a>
             </div>
         </form>
